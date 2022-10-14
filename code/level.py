84453b0e883f5b9eb71bc5e384f562b347337d36
AdamUhh <adamuhh.dev@gmail.com>
2022-10-14 23:36:06 +0400

    Added collisions, player animations and basic player weapon

--- code/level.py
+++ code/level.py
@@ -1,8 +1,11 @@
+from random import choice
 import pygame
 from settings import *
 from tile import Tile
 from player import Player
 from debug import debug
+from support import *
+from weapon import Weapon
 
 
 class Level:
@@ -15,24 +18,52 @@ class Level:
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
 
+        # ? attack sprites
+        self.current_attack = None
+
         # sprite setup
         self.create_map()
 
     def create_map(self):
-        # ? enumerate provides the index
-        for row_index, row in enumerate(WORLD_MAP):  # ? y coords
-            for col_index, col in enumerate(row):  # ? x coords
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    # ? Create a rock tile ((position), [groups it will be in])
-                    # ? a col that is an 'x' will be visible to the player, but also acts as an obstacle
-                    # ? that the player can collide with (essentially a wall)
-                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
-                if col == 'p':
-                    # ? player
-                    self.player = Player(
-                        (x, y), [self.visible_sprites], self.obstacle_sprites)
+        layouts = {
+            'boundary': import_csv_layout('../map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('../map/map_Grass.csv'),
+            'object': import_csv_layout('../map/map_Objects.csv')
+        }
+        graphics = {
+            'grass': import_folder('../graphics/grass'),
+            'objects': import_folder('../graphics/objects')
+        }
+
+        for style, layout in layouts.items():
+            # ? enumerate provides the index
+            for row_index, row in enumerate(layout):  # ? y coords
+                for col_index, col in enumerate(row):  # ? x coords
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            # ? create a collision tile
+                            Tile((x, y), [self.obstacle_sprites], 'invisible')
+                        if style == 'grass':
+                            # ? create a grass tile
+                            random_grass_image = choice(graphics['grass'])
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'grass', random_grass_image)
+                        if style == 'object':
+                            # ? create a object tile
+                            object_surface = graphics['objects'][int(col)]
+                            Tile((x, y), [self.visible_sprites, self.obstacle_sprites], 'object', object_surface)
+
+        self.player = Player(
+            (2000, 1300), [self.visible_sprites], self.obstacle_sprites, self.create_attack, self.destroy_attack)
+
+    def create_attack(self):
+        self.current_attack = Weapon(self.player, [self.visible_sprites])
+
+    def destroy_attack(self):
+        if self.current_attack:
+            self.current_attack.kill()
+        self.current_attack = None
 
     def run(self):
         # update and draw the game
@@ -49,12 +80,21 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
+        # ? creating the floor
+        self.floor_surface = pygame.image.load(
+            '../graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surface.get_rect(topleft=(0, 0))
+
     def custom_draw(self, player):
         # ? getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
 
-        # ? displays the sprites on the screen, sprites that have a bigger 'y' value
+        # ? drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surface, floor_offset_pos)
+
+        # ? displays the sprites on the screen by their y position, sprites that have a bigger 'y' value
         # ? will be displayed ontop. This ensures that the player sprite is overlayed properly
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
